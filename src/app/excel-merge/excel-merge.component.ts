32e7c84c981e33e9c62e8c68ff941065d5e3ac25
Hemanth <hemanthhkhk7@gmail.com>
2025-08-11 18:51:34 +0530

    changes

--- src/app/excel-merge/excel-merge.component.ts
+++ src/app/excel-merge/excel-merge.component.ts
@@ -1,44 +1,19 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { ExcelMergeService } from '../excel-merge.service';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+// import { ExcelMergeService } from '../excel-merge.service';
 
 @Component({
   selector: 'app-excel-merge',
   templateUrl: './excel-merge.component.html',
   styleUrls: ['./excel-merge.component.scss']
 })
-export class ExcelMergeComponent {
-  @ViewChild('fileInput', { static: false }) fileInput!: ElementRef;
-  mergedFileUrl: string | null = null;
-  selectedFiles: string[] = [];
+export class ExcelMergeComponent implements OnInit {
 
-  constructor(private excelMergeService: ExcelMergeService) { }
-
-  onFileChange(event: any): void {
-    const files: File[] = Array.from(event.target.files);
-    this.selectedFiles = files.map(file => file.name);
-
-    this.excelMergeService.mergeExcelFiles(files)
-      .then(mergedBlob => {
-        this.mergedFileUrl = URL.createObjectURL(mergedBlob);
-      })
-      .catch(error => console.error('Error merging files:', error));
-  }
-
-  downloadMergedFile(): void {
-    if (this.mergedFileUrl) {
-      const a = document.createElement('a');
-      a.href = this.mergedFileUrl;
-      a.download = 'merged_output.xlsx';
-      a.click();
-      this.clearFileInput();
-    }
-  }
-
-  clearFileInput(): void {
-    if (this.fileInput) {
-      this.fileInput.nativeElement.value = '';
-      this.mergedFileUrl = '';
-      this.selectedFiles = [];
-    }
+  ngOnInit(): void { }
+  
+  showSection(id: any) {
+    console.log("id", id);
+    document.querySelectorAll('.section').forEach((sec: any) => sec.style.display = 'none');
+    const section = document.getElementById(id)!;
+    section.style.display = 'block';
   }
 }
